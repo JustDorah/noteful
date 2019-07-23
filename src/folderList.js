b37eff739e88f18ddb2a40b42a57ffd9b274d116
Dorah <dorah.bmw@gmail.com>
2019-07-22 22:14:57 -0500

    modified folderList

--- src/folderList.js
+++ src/folderList.js
@@ -5,8 +5,8 @@ export default function FolderList(props) {
   //const notes = props.notes;
   //console.log(notes, "the notes");
   return (
-    <div>
-      <div className="List">{props.mainFolders}</div>
+    <div className="List">
+      <div>{props.mainFolders}</div>
 
       <div className="fButton"> Add Folder </div>
     </div>
