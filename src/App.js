4b698524214ce7c105b8d95ef5f6553c2d317ac2
Dorah <dorah.bmw@gmail.com>
2019-07-24 04:26:11 -0500

    created folderjs&css, added links to each note and folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import NOTES from "./noteStore";
 import MainPage from "./main";
 import NoteList from "./noteList";
@@ -37,7 +38,9 @@ export default class App extends Component {
       //console.log(date);
       return (
         <div className="eachNote" key={note.id}>
-          <h2>{note.name}</h2>
+          <h2>
+            <Link to={`/note/${note.id}`}>{note.name}</Link>
+          </h2>
           <p>Date modified on {date}</p>
           <p />
           <div className="removeNoteButton" key={note.id}>
@@ -51,9 +54,10 @@ export default class App extends Component {
     const mainFolders = dummyFolders.map(folder => {
       return (
         <p className="eachFolder" key={folder.id}>
-          <a href="folder.name" key={folder.id}>
+          {/* <a href="folder.name" key={folder.id}>
             {folder.name}
-          </a>
+          </a> */}
+          <Link to={`/folder/${folder.id}`}>{folder.name}</Link>{" "}
         </p>
       );
     });
---
+++ src/folder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import NOTES from "./noteStore";
+import "./folder.css";
+
+export default function Folder(props) {
+  //const notes = props.notes;
+  //console.log(notes, "the notes");
+  return (
+    <div className="folder">
+      <div>{/* stuff */}</div>
+
+      <div className="fButton">{/* stuff */}</div>
+    </div>
+  );
+}
