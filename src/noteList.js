05b2425e453629fa1dbad6d0017b0e0f5ab66780
Dorah <dorah.bmw@gmail.com>
2019-07-28 22:27:06 -0500

    fixed mistaken undo

--- src/noteList.js
+++ src/noteList.js
@@ -5,12 +5,6 @@ export default function NoteList(props) {
   //console.log(notes, "the notes");
   return (
     <div>
-      {/* <div className="sidebar">
-        <a href="#folder"> Folder</a>
-        <a href="#folder1"> Folder1</a>
-        <a href="#folder2"> Folder2</a>
-      </div> */}
-
       <div className="List">{props.mainNotes}</div>
 
       <div className="nButton">Add note</div>
