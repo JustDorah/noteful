1c2d1ea2e39b4faab0a4b8e69b575526490b171f
Dorah <dorah.bmw@gmail.com>
2019-08-02 04:30:27 -0500

    fetched notes&folders in App.js added context to folderlist.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NotefulContext from "./NotefulContext";
 //import NOTES from "./noteStore";
 import MainPage from "./mainPage";
 //import NoteList from "./noteList";
+import config from "./config";
 
 import "./App.css";
 //import Folder from "./folder";
@@ -29,6 +30,59 @@ export default class App extends Component {
       selectedFolder: clickedFolder
     });
   };
+  /**Store fetch response for folders and notes */
+
+  setFolder = ApiFolder => {
+    this.setState({
+      ApiFolder,
+      error: null
+    });
+    console.log(ApiFolder, "APP ApiFolder");
+  };
+
+  setNote = ApiNotes => {
+    this.setState({
+      ApiNotes,
+      error: null
+    });
+    console.log(ApiNotes, "APP ApiNotes");
+  };
+
+  /**Get/Fetch folders and notes */
+  componentDidMount() {
+    //Get folder data
+    fetch(config.API_FOLDERS, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then(this.setFolder)
+      .catch(error => this.setState({ error }));
+
+    //get note data
+    fetch(config.API_NOTES, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json"
+        // Authorization: `Bearer ${config.API_KEY}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then(this.setNote)
+      .catch(error => this.setState({ error }));
+  }
 
   render() {
     //get & show notes data
--- src/folderList.js
+++ src/folderList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./folderList.css";
+import NotefulContext from "./NotefulContext";
 //import Folder from "./folder";
 
 export default function FolderList(props) {
   return (
-    <div className="List">
-      <div>{props.mainFolders}</div>
-      <div className="fButton"> Add Folder </div>
-    </div>
+    <NotefulContext.Consumer>
+      {context => (
+        <div className="List">
+          <div>{props.mainFolders}</div>
+          <div className="fButton"> Add Folder </div>
+        </div>
+      )}
+    </NotefulContext.Consumer>
   );
 }
