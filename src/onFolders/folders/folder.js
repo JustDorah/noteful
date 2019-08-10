13f6f359cc6bc073557a4520f6b16988416eb604
Dorah <dorah.bmw@gmail.com>
2019-08-10 16:32:36 -0500

    folder format

--- src/onFolders/folders/folder.js
+++ src/onFolders/folders/folder.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 
 export default class Folder extends Component {
   render() {
+    const { fName, key } = this.props;
+
     return (
       <div className="Folder">
         {/* stuff */}
-        FOLDER
+        <h3>FOLDER</h3>
+        <div
+          className="eachFolder"
+          key={key}
+          data-div_id={key}
+          // onClick={e => setSelectedFolder(folder.id)}
+        />
+        {fName}
       </div>
     );
   }
